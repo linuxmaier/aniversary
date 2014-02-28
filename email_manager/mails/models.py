6eed6fe2a8cbfd48e1f8d1d862082a9a4a22ef5b
linuxmaier <andrew.eric.maier@gmail.com>
2014-02-27 21:23:40 -0600

    updates, added south

--- email_manager/mails/models.py
+++ email_manager/mails/models.py
@@ -2,16 +2,19 @@ from django.db import models
 
 # Create your models here.
 
-#class Person(models.Model):
-#	full_name = models.CharField(max_length=20)
-#	first_name = models.CharField(max_length=20)
-#	last_name = models.CharField(max_length=20)
+class Person(models.Model):
+	first_name = models.CharField(max_length=20)
+	last_name = models.CharField(max_length=20)
+	email_addresses = models.CharField(max_length=50)
+
+	def __unicode__(self):
+		return first_name + last_name
 
 class Message(models.Model):
 	from_address = models.EmailField()
-#	from_person = models.ForeignKey(Person)
+	from_person = models.ForeignKey(Person, related_name='sent_mail')
 	to_address = models.EmailField()
-#	to_person = models.ForeignKey(Person)
+	to_person = models.ForeignKey(Person, related_name='received_mail')
 	sent_date = models.DateTimeField('date sent')
 	subject = models.CharField(max_length=50)
 	message = models.TextField()
